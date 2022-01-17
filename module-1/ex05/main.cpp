6ee7d894d1e9d1d5463066296ec52049ad44b438
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2022-01-17 02:07:42 +0100

    Karen exresice completed

---
+++ module-1/ex05/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Karen.hpp"
+
+int     main() {
+    Karen karen;
+    karen.complain("debug");
+    karen.complain("info");
+    karen.complain("warning");
+    karen.complain("error");
+    return 0;
+}
