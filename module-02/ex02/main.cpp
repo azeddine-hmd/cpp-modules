2ccc5f41a52f4fe902e780f74a72618bbd6ecfe1
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2022-02-05 19:02:05 +0100

    fix fixed point for b (5.06 -> 5.05) now output correct and same as subject output

--- module-02/ex02/main.cpp
+++ module-02/ex02/main.cpp
@@ -3,7 +3,7 @@
 
 int main( void ) {
     Fixed       a;
-    Fixed const b( Fixed( 5.06f ) * Fixed( 2 ) );
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
     std::cout << a << std::endl;
     std::cout << ++a << std::endl;
@@ -14,7 +14,6 @@ int main( void ) {
     std::cout << b << std::endl;
 
     std::cout << Fixed::max( a, b ) << std::endl;
-    std::cout << Fixed::min( a, b ) << std::endl;
 
     return 0;
 }
