a31a029e409fd4cda8b10be3e376b62705abb7c4
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2022-03-14 07:25:06 +0100

    ex02 identifying type using dynamic_cast

---
+++ module-06/ex02/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <typeinfo>
+
+class Base {
+public:
+    virtual ~Base() {}
+};
+class A : public Base {};
+class B : public Base {};
+class C : public Base {};
+
+Base *generate() {
+    Base *base = NULL;
+
+    std::srand(std::time(NULL));
+    int choice = std::rand() % 3;
+
+    switch (choice) {
+        case 0:
+            base = new A();
+            break;
+        case 1:
+            base = new B();
+            break;
+        case 2:
+            base = new C();
+            break;
+    }
+
+    return base;
+}
+
+void identify(Base *p) {
+    std::cout << "using pointer, the type is: ";
+    if (dynamic_cast<A*>(p)) {
+        std::cout << "A" << std::endl;
+    } else if (dynamic_cast<B*>(p)) {
+        std::cout << "B" << std::endl;
+    } else if (dynamic_cast<C*>(p)) {
+        std::cout << "C" << std::endl;
+    } else {
+        std::cout << "unknown" << std::endl;
+    }
+}
+
+void identify(Base &p) {
+    std::cout << "using reference, the type is: ";
+
+    try {
+        dynamic_cast<A&>(p);
+        std::cout << "A" << std::endl;
+    } catch (std::bad_cast &e) {
+
+        try {
+            dynamic_cast<B&>(p);
+            std::cout << "B" << std::endl;
+        } catch (std::bad_cast &e) {
+
+            try {
+                dynamic_cast<C&>(p);
+                std::cout << "C" << std::endl;
+            } catch (std::bad_cast &e) {
+                std::cout << "unknown" << std::endl;
+            }
+
+        }
+
+    }
+}
+
+int main() {
+    Base *b = generate();
+    if (!b)
+        std::cout << "unexpected type have been created!" << std::endl;
+    identify(b);
+    identify(*b);
+    return 0;
+}
+
